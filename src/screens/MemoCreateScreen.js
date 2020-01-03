a2edb94479bd2ae6fce38e24bf8663d1c235094a
amok6from7 <kwifi7@gmail.com>
2020-01-03 22:04:03 +0900

    fix keyboard action

--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 import firebase from 'firebase';
 
 import CircleButton from '../elements/CircleButton';
@@ -25,7 +25,7 @@ class MemoCreateScreen extends React.Component {
 
     render() {
       return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={80}>
           <TextInput
             style={styles.memoEditInput}
             multiline
@@ -37,7 +37,7 @@ class MemoCreateScreen extends React.Component {
             name="check"
             onPress={() => { this.handlePress(); }}
           />
-        </View>
+        </KeyboardAvoidingView>
       );
     }
 }
